6407ff26414f2d1e3cf981021eb417e7c32552f8
z80lives <shath.ibrahim@gmail.com>
2022-10-18 22:45:21 +0800

    Tool update

---
+++ tools/api/models/user.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface User {
+  email: string;
+  fullname: string;
+  password?: string;
+  phone?: string;
+  username: string;
+}
---
+++ tools/api/services/auth-controller.service.ts
@@ -0,0 +1,78 @@
+/* tslint:disable */
+/* eslint-disable */
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpContext } from '@angular/common/http';
+import { BaseService } from '../base-service';
+import { ApiConfiguration } from '../api-configuration';
+import { StrictHttpResponse } from '../strict-http-response';
+import { RequestBuilder } from '../request-builder';
+import { Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthControllerService extends BaseService {
+  constructor(
+    config: ApiConfiguration,
+    http: HttpClient
+  ) {
+    super(config, http);
+  }
+
+  /**
+   * Path part for operation authControllerCreate
+   */
+  static readonly AuthControllerCreatePath = '/auth/register';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `create()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  create$Response(params?: {
+    context?: HttpContext
+    body?: {
+}
+  }
+): Observable<StrictHttpResponse<User>> {
+
+    const rb = new RequestBuilder(this.rootUrl, AuthControllerService.AuthControllerCreatePath, 'post');
+    if (params) {
+      rb.body(params.body, 'application/json');
+    }
+
+    return this.http.request(rb.build({
+      responseType: 'json',
+      accept: 'application/json',
+      context: params?.context
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return r as StrictHttpResponse<User>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `create$Response()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  create(params?: {
+    context?: HttpContext
+    body?: {
+}
+  }
+): Observable<User> {
+
+    return this.create$Response(params).pipe(
+      map((r: StrictHttpResponse<User>) => r.body as User)
+    );
+  }
+
+}
